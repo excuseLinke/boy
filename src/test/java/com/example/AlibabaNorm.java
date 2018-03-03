b7cf3f6e0c4a162e97b89e6532bac1253eb0e865
excuseLinKe <104081131@qq.com>
2018-03-03 10:30:12 +0800

    测试

--- src/test/java/com/example/AlibabaNorm.java
+++ src/test/java/com/example/AlibabaNorm.java
@@ -1,7 +1,26 @@
 package com.example;
 
+import java.time.Instant;
+
 /**
  * Created by linke on 2018/3/3.
  */
 public class AlibabaNorm {
+    private static Object time;
+
+    public static void main(String[] args) {
+    }
+    //比较
+    private static int get(int a, int b) {
+        return a > b ? 1:(a == b ? 0 : -1);
+    }
+
+    /**
+     * {@link java.time.Instant}
+     * thread-safe的工具类，和 Date类似
+     * {@link java.util.Date}
+     */
+    public static void getTime() {
+        Instant.now().getEpochSecond();
+    }
 }
