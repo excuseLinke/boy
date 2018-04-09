21edecd5d106879c5865fa0f160aa4b8ba96b0f1
excuseLinKe <104081131@qq.com>
2018-04-09 20:53:43 +0800

    测试

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,17 +2,15 @@ package com.example;
 
 import com.example.mapper.UserMapper;
 import com.example.pojo.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
+
 
 /**
  * Created by linke on 2016/12/31.
  */
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -20,14 +18,19 @@ public class HelloController {
     private UserMapper userMapper;
     @Autowired
     private BoyProperties boyProperties;
-    private  static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+  //  private  static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String get(ModelAndView modelAndView) {
+
+    public String get(@ModelAttribute TestDemo boy, @RequestParam String id) {
+        System.out.println(id);
+        System.out.println(boy.getAge());
         System.out.println(boyProperties.getWeight());
         User user = userMapper.selectUser();
-        modelAndView.addObject("list-test",user);
-        modelAndView.setViewName("list");
-        logger.info("this is id:{} count:{}",user.getId(),user.getUserCont());
+       // logger.info("this is id:{} count:{}",user.getId(),user.getUserCont());
+        log.info("this is id:{} count:{}",user.getId(),user.getUserCont());
         return "Hello Spring Boot！" + user.getUserCont()+ user.getUserCont();
     }
 }
